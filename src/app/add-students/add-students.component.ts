a14d053a0ea5733e9b0e0c5d686c5303823e0030
japps1c07 <japps1c07@cicct.local>
2017-08-30 10:37:26 +0800

    Update4.0
    gg

--- src/app/add-students/add-students.component.ts
+++ src/app/add-students/add-students.component.ts
@@ -7,13 +7,6 @@ import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 })
 export class AddStudentsComponent implements OnInit {
 
-  @Output() Clicked = new EventEmitter<any>();
-  @Input() message;
-  flag = 'input';
-
-  input(){
-    this.Clicked.emit({mode: this.flag});
-  }
   constructor() { }
 
   ngOnInit() {
